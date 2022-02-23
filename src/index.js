661807b7d75c7b1d67b03c903160d0137be25f6f
AnnaBorisenko13 <ann.jeferson@gmail.com>
2022-02-24 00:49:39 +0300

    feat: add decision

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function reverse (n) {
-  
+const module = Math.abs(n);
+ var string = module.toString();
+  const nLength = string.length;
+  let reverseArr = [];
+
+  for (i = 0; i < nLength; i++) {  //проходимся по строке начиная с нулевого символа т.е. с сотен
+    let ints = string[i].split('').reverse().map(parseFloat);
+    console.log(ints);
+    reverseArr.unshift(ints);
+  }
+  return Number(reverseArr.join('')); 
 }
